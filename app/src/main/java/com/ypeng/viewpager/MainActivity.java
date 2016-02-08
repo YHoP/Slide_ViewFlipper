8b68b083ea9dbb0568850ab4fb07eb7a34910d5b
YHop <ypeng.tw@gmail.com>
2016-02-08 10:24:10 -0800

    Refactor the file names

--- app/src/main/java/com/ypeng/viewpager/MainActivity.java
+++ app/src/main/java/com/ypeng/viewpager/MainActivity.java
@@ -1,67 +1,51 @@
 package com.ypeng.viewpager;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ViewFlipper;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private PagerAdapter mPagerAdapter;
-    private ViewPager mViewPager;
-    private int mCurrentPosition = 0;
+    private Animation mInFromRight;
+    private Animation mOutToLeft;
+    private Animation mInFromLeft;
+    private Animation mOutToRight;
+    private ViewFlipper mViewFlipper;
     private Timer mTimer;
     private Handler mHandler;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initTimer();
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        setUpViewPager();
-
-        mViewPager.setOnTouchListener(new OnSwipeTouchListener(this) {
-            public void onSwipeRight() {
-                if(mCurrentPosition == 0 ) {
-                    mCurrentPosition = mPagerAdapter.getCount();
-                }
-                mViewPager.setCurrentItem(mCurrentPosition, true);
-            }
-
-            public void onSwipeLeft() {
-                if(mCurrentPosition < mPagerAdapter.getCount() - 1) {
-                    mCurrentPosition++;
-                } else {
-                    mCurrentPosition = 0;
-                }
-                mViewPager.setCurrentItem(mCurrentPosition, true);
-            }
-        });
-    }
-
-    private void initTimer() {
         mTimer = new Timer();
         mHandler = new Handler();
+
+        mViewFlipper = (ViewFlipper) findViewById(R.id.view_flipper);
+        mViewFlipper.setDisplayedChild(0);
+        initAnimations();
+        setAutoPlay();
+
     }
 
-    private void setUpViewPager() {
-        mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), this);
-        mViewPager.setAdapter(mPagerAdapter);
-        mViewPager.setCurrentItem(mCurrentPosition);
-
+    private void setAutoPlay(){
         final Runnable changeCurrentImage = new Runnable() {
             @Override
             public void run() {
-                if(mCurrentPosition < mPagerAdapter.getCount() - 1) {
-                    mCurrentPosition++;
-                } else {
-                    mCurrentPosition = 0;
-                }
-                mViewPager.setCurrentItem(mCurrentPosition, true);
+                mViewFlipper.setInAnimation(mInFromRight);
+                mViewFlipper.setOutAnimation(mOutToLeft);
+                mViewFlipper.showNext();
             }
         };
 
@@ -73,23 +57,73 @@ public class MainActivity extends AppCompatActivity {
         }, 3000, 3000);
     }
 
-//    @Override
-//    public void onPageScrollStateChanged(int state) {
-//        if (state == ViewPager.SCROLL_STATE_IDLE) {
-//            mCurrentPosition = mViewPager.getCurrentItem();
-//            initTimer();
-//        }
-//    }
-//
-//    @Override
-//    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//        mCurrentPosition = position;
-//        initTimer();
-//    }
-//
-//    @Override
-//    public void onPageSelected(int position) {
-//        mCurrentPosition = position;
-//        initTimer();
-//    }
+    private void initAnimations() {
+        mInFromRight = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                +1.0f, Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        mInFromRight.setDuration(500);
+        AccelerateInterpolator accelerateInterpolator = new AccelerateInterpolator();
+        mInFromRight.setInterpolator(accelerateInterpolator);
+
+        mInFromLeft = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                -1.0f, Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        mInFromLeft.setDuration(500);
+        mInFromLeft.setInterpolator(accelerateInterpolator);
+
+        mOutToRight = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,
+                0.0f, Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        mOutToRight.setDuration(500);
+        mOutToRight.setInterpolator(accelerateInterpolator);
+
+        mOutToLeft = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        mOutToLeft.setDuration(500);
+        mOutToLeft.setInterpolator(accelerateInterpolator);
+
+        final GestureDetector gestureDetector;
+        gestureDetector = new GestureDetector(new MyGestureDetector());
+
+        mViewFlipper.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                if (gestureDetector.onTouchEvent(event)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });
+    }
+
+    private class MyGestureDetector extends GestureDetector.SimpleOnGestureListener {
+
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 250;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                               float velocityY) {
+            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                return false;
+            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                mViewFlipper.setInAnimation(mInFromRight);
+                mViewFlipper.setOutAnimation(mOutToLeft);
+                mViewFlipper.showNext();
+            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                mViewFlipper.setInAnimation(mInFromLeft);
+                mViewFlipper.setOutAnimation(mOutToRight);
+                mViewFlipper.showPrevious();
+            }
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
 }
---
+++ app/src/main/java/com/ypeng/viewpager/ViewPagerctivity.java
@@ -0,0 +1,95 @@
+package com.ypeng.viewpager;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ViewPagerctivity extends AppCompatActivity {
+
+    private PagerAdapter mPagerAdapter;
+    private ViewPager mViewPager;
+    private int mCurrentPosition = 0;
+    private Timer mTimer;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpager);
+        initTimer();
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        setUpViewPager();
+
+        mViewPager.setOnTouchListener(new OnSwipeTouchListener(this) {
+            public void onSwipeRight() {
+                if(mCurrentPosition == 0 ) {
+                    mCurrentPosition = mPagerAdapter.getCount();
+                }
+                mViewPager.setCurrentItem(mCurrentPosition, true);
+            }
+
+            public void onSwipeLeft() {
+                if(mCurrentPosition < mPagerAdapter.getCount() - 1) {
+                    mCurrentPosition++;
+                } else {
+                    mCurrentPosition = 0;
+                }
+                mViewPager.setCurrentItem(mCurrentPosition, true);
+            }
+        });
+    }
+
+    private void initTimer() {
+        mTimer = new Timer();
+        mHandler = new Handler();
+    }
+
+    private void setUpViewPager() {
+        mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), this);
+        mViewPager.setAdapter(mPagerAdapter);
+        mViewPager.setCurrentItem(mCurrentPosition);
+
+        final Runnable changeCurrentImage = new Runnable() {
+            @Override
+            public void run() {
+                if(mCurrentPosition < mPagerAdapter.getCount() - 1) {
+                    mCurrentPosition++;
+                } else {
+                    mCurrentPosition = 0;
+                }
+                mViewPager.setCurrentItem(mCurrentPosition, true);
+            }
+        };
+
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mHandler.post(changeCurrentImage);
+            }
+        }, 3000, 3000);
+    }
+
+//    @Override
+//    public void onPageScrollStateChanged(int state) {
+//        if (state == ViewPager.SCROLL_STATE_IDLE) {
+//            mCurrentPosition = mViewPager.getCurrentItem();
+//            initTimer();
+//        }
+//    }
+//
+//    @Override
+//    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//        mCurrentPosition = position;
+//        initTimer();
+//    }
+//
+//    @Override
+//    public void onPageSelected(int position) {
+//        mCurrentPosition = position;
+//        initTimer();
+//    }
+}
