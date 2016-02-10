5e26a8ebdeab2faf9582549dae0c65c0d84eb1bc
YHop <ypeng.tw@gmail.com>
2016-02-10 11:13:23 -0800

    Create Team model

---
+++ app/src/main/java/com/ypeng/viewpager/Team.java
@@ -0,0 +1,26 @@
+package com.ypeng.viewpager;
+
+public class Team {
+    private String image;
+    private String link;
+
+    public Team(){
+        // empty default constructor
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
